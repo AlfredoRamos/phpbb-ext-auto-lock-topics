b4a33c5e781fc55e95ee35a6d7affde2567d24d3
Alfredo Ramos <alfredo.ramos@yandex.com>
2017-05-06 17:59:47 -0500

    Add Spanish translations
    - Formal honorifics [es]
    - Casual honorifics [ex_x_tu]

---
+++ language/es/autolocktopics.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Auto-lock Topics Extension for phpBB.
+ * @author Alfredo Ramos <alfredo.ramos@yandex.com>
+ * @copyright 2017 Alfredo Ramos
+ * @license GNU GPL-2.0
+ */
+
+/**
+ * @ignore
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+ * @ignore
+ */
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+
+$lang = array_merge($lang, [
+	'ACP_FORUM_AUTO_LOCK_SETTINGS'	=> 'Preferencias del cerrado automático',
+	'ACP_ENABLE_AUTO_LOCK'			=> 'Habilitar cierre automático',
+	'ACP_AUTO_LOCK_ANNOUNCEMENTS'	=> 'Cierre automático de anuncios',
+	'ACP_AUTO_LOCK_STICKIES'		=> 'Cierre automático de notas',
+	'ACP_AUTO_LOCK_POLLS'			=> 'Cierre automático de encuestas',
+	'ACP_AUTO_LOCK_DAYS'			=> 'Vigencia de temas',
+	'ACP_AUTO_LOCK_DAYS_EXPLAIN'	=> 'Número de días que se mantendrá el tema sin mensajes nuevos antes del cierre automático.',
+	'ACP_AUTO_LOCK_FREQUENCY'		=> 'Frecuencia del cierre automático',
+	'ACP_AUTO_LOCK_FREQUENCY_EXPLAIN' => 'Tiempo en días transcurrido entre los cierres automáticos de temas.',
+
+	'LOG_AUTO_LOCK_TOPIC'	=> '<strong>Temas cerrados automáticamente</strong><br />» [%d] %s'
+]);
