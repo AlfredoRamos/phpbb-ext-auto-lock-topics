851c4284d551745457358ebf492c469c3861d0bf
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2019-12-09 12:03:32 -0600

    Update tests

--- tests/event/listener_test.php
+++ tests/event/listener_test.php
@@ -22,7 +22,7 @@ class listener_test extends phpbb_test_case
 	/** @var \alfredoramos\autolocktopics\includes\helper */
 	protected $helper;
 
-	public function setUp()
+	public function setUp(): void
 	{
 		parent::setUp();
 
