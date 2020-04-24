174a7ef00611177e28b7c3c31a9b8cc10bc7c522
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2020-04-24 13:50:39 -0500

    New translations info_acp_forums.php (Portuguese, Brazilian)

--- language/pt_br/acp/info_acp_forums.php
+++ language/pt_br/acp/info_acp_forums.php
@@ -24,7 +24,7 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, [
-	'ACP_FORUM_AUTO_LOCK_SETTINGS'	=> 'Configurações Auto-trancar',
+	'ACP_FORUM_AUTO_LOCK_SETTINGS'	=> 'Configurações do Auto-trancar',
 	'ACP_ENABLE_AUTO_LOCK'			=> 'Ativar auto-trancar',
 	'ACP_AUTO_LOCK_ANNOUNCEMENTS'	=> 'Auto-trancar anúncio',
 	'ACP_AUTO_LOCK_STICKIES'		=> 'Auto-trancar fixos',
@@ -32,9 +32,9 @@ $lang = array_merge($lang, [
 	'ACP_AUTO_LOCK_POLLS_EXPLAIN'	=> 'Trancar tópicos com enquetes não votadas baseadas na idade do post em dias.',
 	'ACP_AUTO_LOCK_DAYS'			=> 'Auto-trancar post por idade',
 	'ACP_AUTO_LOCK_DAYS_EXPLAIN'	=> 'Número de dias desde o último post.',
-	'ACP_AUTO_LOCK_FREQ'		=> 'Freqüência do Auto-trancar',
+	'ACP_AUTO_LOCK_FREQ'		=> 'Frequência do Auto-trancar',
 	'ACP_AUTO_LOCK_FREQ_EXPLAIN' => 'Tempo em dias entre os eventos do Auto-trancar.',
 
 	// %s => Forum name
-	'LOG_AUTO_LOCK_TOPIC'	=> '<strong>Tópicos auto-trancados</strong><br />» %s'
+	'LOG_AUTO_LOCK_TOPIC'	=> '<strong>Tópicos trancados automaticamente</strong><br />» %s'
 ]);
